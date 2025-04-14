e7c0ecac06cf84a1a3118197a139d735c24b70d7
bluemin <zeem_m2@naver.com>
2025-04-14 12:56:11 +0900

    feat: IntroductionBtns 레이아웃 추가

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import '../styles/components/Introduction.scss';
+import IntroductionBtns from './IntroDuctionBtns';
 
 const Introduction = () => {
   return (
@@ -9,12 +10,17 @@ const Introduction = () => {
         <h1>
           Hello I&apos;m <br /> <span>Kim Jimin</span>
         </h1>
+        <p>
+          I&apos;m a frontend developer who keeps learning, <br />
+          creating, and exploring the endless ocean of the web
+        </p>
+
+        <IntroductionBtns />
       </div>
       <div className="introduction-image-box">
         <Image
           src="/profile.webp"
           quality={100}
-          //   fill
           alt="profile image"
           className="profile-image"
           width={300}
---
+++ src/components/IntroductionBtns.tsx
@@ -0,0 +1,12 @@
+const IntroductionBtns = () => {
+  return (
+    <div className="introduction-btns-container">
+      <button>Download CV</button>
+      <button>github</button>
+      <button>notion</button>
+      <button>blog</button>
+    </div>
+  );
+};
+
+export default IntroductionBtns;
