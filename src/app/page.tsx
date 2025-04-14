e751222e658ff6b6ca7d307da9914748e2ca316f
bluemin <zeem_m2@naver.com>
2025-04-14 12:57:14 +0900

    design: font-size 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
+import CodeSnap from '@/components/CodeSnap';
 import Introduction from '@/components/Introduction';
 
 import '@/styles/Home.scss';
+import AboutMe from './about_me/page';
 
 export default function Home() {
   return (
     <div className="home-wrapper">
       <Introduction />
+      <CodeSnap />
+      <AboutMe />
     </div>
   );
 }
