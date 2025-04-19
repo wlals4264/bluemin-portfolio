5bfe11acb05c87ee20f8e5976ee434d352175e84
bluemin <zeem_m2@naver.com>
2025-04-19 19:58:35 +0900

    Refactor: 어바웃 미 컴포넌트 간격 및 줄바꿈 리팩토링

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -13,7 +13,17 @@ const myInfoData = [
   { title: '위치', content: '서울시 동작구', icon: <MdPlace /> },
   { title: '연락처', content: '010-7369-0111', icon: <FaPhone /> },
   { title: '이메일', content: 'zeem_m2@naver.com', icon: <IoIosMail /> },
-  { title: '학력', content: '숙명여자대학교(작곡과 & 문화예술기획)', icon: <RiGraduationCapFill /> },
+  {
+    title: '학력',
+    content: (
+      <>
+        숙명여자대학교
+        <br />
+        (작곡과 & 문화예술기획)
+      </>
+    ),
+    icon: <RiGraduationCapFill />,
+  },
 ];
 const AboutMe = () => {
   return (
