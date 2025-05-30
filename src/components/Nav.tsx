ba0b1465a221d5ea974bdddc77ef2e142c29c80b
bluemin <zeem_m2@naver.com>
2025-05-31 01:41:58 +0900

    Design: 헤더 반응형 구현

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,4 +1,8 @@
+'use client';
+
 import '../styles/components/Nav.scss';
+import { useState } from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
 
 type TitleItem = {
   label: string;
@@ -13,19 +17,26 @@ type NavProps = {
 };
 
 export default function Nav({ titles, sectionRefs }: NavProps) {
+  const [isNavOpen, setIsNavOpen] = useState<boolean>(false);
   const handleClick = (title: string) => {
     const targetRef = sectionRefs[title];
     targetRef?.current?.scrollIntoView({ behavior: 'smooth' });
   };
 
   return (
-    <nav className="nav-container">
-      {titles.map(({ label, key }) => (
-        <span key={key} onClick={() => handleClick(key)}>
-          {label}
-        </span>
-      ))}
-      <button>Hire me!</button>
-    </nav>
+    <>
+      <nav className={`nav-container ${isNavOpen ? 'open' : ''}`}>
+        {titles.map(({ label, key }) => (
+          <span key={key} onClick={() => handleClick(key)}>
+            {label}
+          </span>
+        ))}
+        <button className="hire-me-btn">Hire me!</button>
+      </nav>
+
+      <button className="nav-toggle" onClick={() => setIsNavOpen((prev) => !prev)} aria-label="toggle nav">
+        <GiHamburgerMenu size={24} />
+      </button>
+    </>
   );
 }
