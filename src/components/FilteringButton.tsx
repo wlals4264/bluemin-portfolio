b6cb876b9f0ce997e83f6cc93485b1a7f4d5668f
bluemin <zeem_m2@naver.com>
2025-06-06 22:58:40 +0900

    Design: 버튼 active 상태 추가

--- src/components/FilteringButton.tsx
+++ src/components/FilteringButton.tsx
@@ -1,6 +1,8 @@
+'use client';
+
 import '../styles/components/FilteringButton.scss';
 import { ProjectCardData } from '@/mocks/projects';
-import React from 'react';
+import React, { useState } from 'react';
 
 interface FilteringButtonProps {
   setFilteredProjects: (projects: ProjectCardData[]) => void;
@@ -8,8 +10,11 @@ interface FilteringButtonProps {
 }
 
 const FilteringButton = ({ projects, setFilteredProjects }: FilteringButtonProps) => {
+  const [activeButton, setActiveButton] = useState<string>('default');
+
   const clickDefaultButton = () => {
     setFilteredProjects([...projects]);
+    setActiveButton('default');
   };
 
   const clickLatestButton = () => {
@@ -24,30 +29,45 @@ const FilteringButton = ({ projects, setFilteredProjects }: FilteringButtonProps
     });
 
     setFilteredProjects(sortedProjects);
+    setActiveButton('latest');
   };
 
   const clickTeamButton = () => {
     const teamProjects = [...projects].filter((project) => project.projectType === 'team');
     setFilteredProjects(teamProjects);
+    setActiveButton('team');
   };
 
   const clickPersonalButton = () => {
     const personalProjects = [...projects].filter((project) => project.projectType === 'personal');
     setFilteredProjects(personalProjects);
+    setActiveButton('personal');
   };
 
   return (
     <div className="filtered-buttons-container">
-      <button className="default-button" type="button" onClick={clickDefaultButton}>
+      <button
+        className={`default-button ${activeButton === 'default' ? 'active' : ''}`}
+        type="button"
+        onClick={clickDefaultButton}>
         기본순
       </button>
-      <button className="latest-button" type="button" onClick={clickLatestButton}>
+      <button
+        className={`latest-button ${activeButton === 'latest' ? 'active' : ''}`}
+        type="button"
+        onClick={clickLatestButton}>
         최신순
       </button>
-      <button className="team-button" type="button" onClick={clickTeamButton}>
+      <button
+        className={`team-button ${activeButton === 'team' ? 'active' : ''}`}
+        type="button"
+        onClick={clickTeamButton}>
         팀 프로젝트
       </button>
-      <button className="personal-button" type="button" onClick={clickPersonalButton}>
+      <button
+        className={`personal-button ${activeButton === 'personal' ? 'active' : ''}`}
+        type="button"
+        onClick={clickPersonalButton}>
         개인 프로젝트
       </button>
     </div>
